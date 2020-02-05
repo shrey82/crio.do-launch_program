8ae027daeeca4ce132f7e3961cacd9b978422b0a
shrey82 <38159468+shrey82@users.noreply.github.com>
2020-02-06 00:23:08 +0530

    Create print_home_directory.sh

---
+++ print_home_directory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this code is contributed by shrey patel 
+# @author: shreypatel
+# TODO: CRIO_TASK_MODULE_LINUX_COMMANDS
+    # Tasks:
+    # 1) Add commands to print the home directory for crio-user
+    # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
+    # 3) Execute the script before submission and confirm the output
+    eval echo "~$crio-user" #prints the home directory of the user named "crio-user".
+    pwd #prints the current working directory of the user.
+    echo "Learn by Doing" #prints the message "Learn by Doing" on the terminal upon exceution.
+    # for exceution write the code as follows: bash filename.sh or sh filename.sh
