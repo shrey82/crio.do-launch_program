196d3afc134b5c505bd21587efa3da8d1e401924
shrey82 <38159468+shrey82@users.noreply.github.com>
2020-02-06 02:05:18 +0530

    Update print_home_directory.sh

--- print_home_directory.sh
+++ print_home_directory.sh
@@ -6,7 +6,6 @@
     # 1) Add commands to print the home directory for crio-user
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output
-    eval echo "~$crio-user" #prints the home directory of the user named "crio-user".
-    pwd #prints the current working directory of the user.
-    echo "Learn by Doing" #prints the message "Learn by Doing" on the terminal upon exceution.
-    # for exceution write the code as follows: bash filename.sh or sh filename.sh
+    eval echo "~$patelshrey115"
+    # pwd
+    # echo "Learn by Doing"
